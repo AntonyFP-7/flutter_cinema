25dffeecd8c8367aa77708a3ef0050af3c070082
AntonyFloresPaz <developer4@traxpark.us>
2025-01-29 14:38:56 -0500

    agregar isart

--- lib/presentation/providers/provider.dart
+++ lib/presentation/providers/provider.dart
@@ -5,3 +5,4 @@ export 'movies/movies_providers.dart';
 export 'movies/movies_repository_provider.dart';
 export 'movies/initial_loading_provider.dart';
 export 'package:cinemapedia/presentation/providers/movies/movies_slideshow_provider.dart';
+export 'package:cinemapedia/presentation/providers/storage/local_storage_provider.dart';
--- lib/presentation/screends/movies/movie_screen.dart
+++ lib/presentation/screends/movies/movie_screen.dart
@@ -48,17 +48,27 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
   }
 }
 
-class _CustomeSliverAppBar extends StatelessWidget {
+class _CustomeSliverAppBar extends ConsumerWidget {
   final Movie movie;
   const _CustomeSliverAppBar({required this.movie});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final size = MediaQuery.of(context).size;
     return SliverAppBar(
       backgroundColor: Colors.black,
       expandedHeight: size.height * 0.7,
       foregroundColor: Colors.white,
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.favorite_border),
+          //icon: const Icon(Icons.favorite_rounded, color: Colors.red),
+          onPressed: () {
+            // Add to favorites
+            ref.watch(localStorageProvider).toggleFavorite(movie);
+          },
+        ),
+      ],
       flexibleSpace: FlexibleSpaceBar(
         titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 2),
         /*   title: Text(
@@ -78,35 +88,22 @@ class _CustomeSliverAppBar extends StatelessWidget {
                 },
               ),
             ),
-            SizedBox.expand(
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      stops: [
-                        0.7,
-                        1.0
-                      ],
-                      colors: [
-                        Colors.transparent,
-                        Colors.black87,
-                      ]),
-                ),
-              ),
+            _CustomGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              stops: [0.0, 0.2],
+              colors: [Colors.black54, Colors.transparent],
             ),
-            SizedBox.expand(
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(begin: Alignment.topLeft, stops: [
-                    0.0,
-                    0.4
-                  ], colors: [
-                    Colors.black87,
-                    Colors.transparent,
-                  ]),
-                ),
-              ),
+            _CustomGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              stops: [0.8, 1.0],
+              colors: [Colors.transparent, Colors.black87],
+            ),
+            _CustomGradient(
+              begin: Alignment.topLeft,
+              stops: [0.0, 0.4],
+              colors: [Colors.black54, Colors.transparent],
             ),
           ],
         ),
@@ -231,6 +228,33 @@ class _ActorsByMovie extends ConsumerWidget {
             ),
           );
         },
+      ),
+    );
+  }
+}
+
+class _CustomGradient extends StatelessWidget {
+  final AlignmentGeometry begin;
+  final AlignmentGeometry end;
+  final List<double> stops;
+  final List<Color> colors;
+  const _CustomGradient(
+      {this.begin = Alignment.centerLeft,
+      this.end = Alignment.centerRight,
+      required this.stops,
+      required this.colors});
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: begin,
+            end: end,
+            stops: stops,
+            colors: colors,
+          ),
+        ),
       ),
     );
   }
