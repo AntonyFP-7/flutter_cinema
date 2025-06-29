7bd2f4fa54a28578d206aa734c47ae00be66ced2
AntonyFloresPaz <developer4@traxpark.us>
2025-06-29 18:25:17 -0500

    `Added check for empty favorite movies list and displayed a message with a button to go to home page`

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -3,6 +3,7 @@ import 'package:cinemapedia/presentation/providers/provider.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
@@ -34,6 +35,39 @@ class FavoritesViewState extends ConsumerState<FavoritesView> {
   @override
   Widget build(BuildContext context) {
     final favoriteMovies = ref.watch(favoriteMovieProvider).values.toList();
+    if (favoriteMovies.isEmpty) {
+      final colors = Theme.of(context).colorScheme;
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.favorite_outline_sharp,
+              size: 60,
+              color: colors.primary,
+            ),
+            Text(
+              'No favorite movies',
+              style: TextStyle(
+                fontSize: 30,
+                color: colors.primary,
+              ),
+            ),
+            Text(
+              'Tap the heart icon to add a movie to your favorites',
+              style: TextStyle(fontSize: 20, color: Colors.black45),
+            ),
+            SizedBox(height: 20),
+            FilledButton.tonal(
+                onPressed: () => context.go('/home/0'),
+                child: Text(
+                  'Go to Home',
+                ))
+          ],
+        ),
+      );
+    }
     return Scaffold(
       body: MovieMasonry(
         loadNextPage: loadNextPage,
